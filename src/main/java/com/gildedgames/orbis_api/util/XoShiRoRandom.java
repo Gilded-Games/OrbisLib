32d4d09338e464e7f6391995330b3c4c09ce8c21
JellySquid <jellysquid@protonmail.com>
2018-12-29 23:01:21 -0600

    performance: Use XoRoShiRo128** instead of XoShiRo256**

--- src/main/java/com/gildedgames/orbis_api/util/XoShiRoRandom.java
+++ src/main/java/com/gildedgames/orbis_api/util/XoShiRoRandom.java
@@ -2,7 +2,7 @@ package com.gildedgames.orbis_api.util;
 
 import java.util.Random;
 
-// XoShiRo256** implementation from DSI Utilities, adopted in a minimal implementation to not
+// XoRoShiRo128** implementation from DSI Utilities, adopted in a minimal implementation to not
 // import Apache Commons.
 //
 // http://xoshiro.di.unimi.it/
@@ -10,7 +10,7 @@ public class XoShiRoRandom extends Random
 {
 	private static final long serialVersionUID = 1L;
 
-	private long s0, s1, s2, s3;
+	private long s0, s1;
 
 	private static final SplitMixRandom seedUniquifier = new SplitMixRandom(System.nanoTime());
 
@@ -39,20 +39,16 @@ public class XoShiRoRandom extends Random
 	@Override
 	public long nextLong()
 	{
-		long result = this.s1;
-		result = Long.rotateLeft(result + (result << 2), 7);
-		result += result << 3;
+		final long s0 = this.s0;
 
-		final long t = this.s1 << 17;
+		long s1 = this.s1;
 
-		this.s2 ^= this.s0;
-		this.s3 ^= this.s1;
-		this.s1 ^= this.s2;
-		this.s0 ^= this.s3;
+		final long result = s0 + s1;
 
-		this.s2 ^= t;
+		s1 ^= s0;
 
-		this.s3 = Long.rotateLeft(this.s3, 45);
+		this.s0 = Long.rotateLeft(s0, 24) ^ s1 ^ s1 << 16;
+		this.s1 = Long.rotateLeft(s1, 37);
 
 		return result;
 	}
@@ -69,7 +65,7 @@ public class XoShiRoRandom extends Random
 		return (int) this.nextLong(n);
 	}
 
-	public long nextLong(final long n)
+	private long nextLong(final long n)
 	{
 		if (n <= 0)
 		{
@@ -80,22 +76,26 @@ public class XoShiRoRandom extends Random
 
 		final long nMinus1 = n - 1;
 
+		// Shortcut for powers of two--high bits
+		if ((n & nMinus1) == 0)
+		{
+			return (t >>> Long.numberOfLeadingZeros(nMinus1)) & nMinus1;
+		}
+
+		// Rejection-based algorithm to get uniform integers in the general case
 		long u = t >>> 1;
 
 		while (u + nMinus1 - (t = u % n) < 0)
 		{
 			u = this.nextLong() >>> 1;
 		}
+
 		return t;
+
 	}
 
 	@Override
 	public double nextDouble()
-	{
-		return (this.nextLong() >>> 11) * 0x1.0p-53;
-	}
-
-	public double nextDoubleFast()
 	{
 		return Double.longBitsToDouble(0x3FFL << 52 | this.nextLong() >>> 12) - 1.0;
 	}
@@ -115,7 +115,7 @@ public class XoShiRoRandom extends Random
 	@Override
 	public void nextBytes(final byte[] bytes)
 	{
-		int i = bytes.length, n = 0;
+		int i = bytes.length, n;
 
 		while (i != 0)
 		{
@@ -126,6 +126,7 @@ public class XoShiRoRandom extends Random
 				bytes[--i] = (byte) bits;
 			}
 		}
+
 	}
 
 	@Override
@@ -135,20 +136,16 @@ public class XoShiRoRandom extends Random
 
 		this.s0 = r.nextLong();
 		this.s1 = r.nextLong();
-		this.s2 = r.nextLong();
-		this.s3 = r.nextLong();
 	}
 
 	public void setState(final long[] state)
 	{
-		if (state.length != 4)
+		if (state.length != 2)
 		{
-			throw new IllegalArgumentException("The argument array contains " + state.length + " longs instead of " + 2);
+			throw new IllegalArgumentException("The argument array contains " + state.length + " longs instead of 2");
 		}
 
 		this.s0 = state[0];
 		this.s1 = state[1];
-		this.s2 = state[2];
-		this.s3 = state[3];
 	}
 }
