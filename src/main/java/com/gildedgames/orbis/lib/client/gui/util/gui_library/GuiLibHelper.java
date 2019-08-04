699bccc7aa794b9d393ae51de38edc2d076e259d
Brandon Pearce <kingbdogz_war3freak@hotmail.com>
2019-08-04 22:58:43 +0800

    Implemented lib helper methods for aligning and min/max area

--- src/main/java/com/gildedgames/orbis/lib/client/gui/util/gui_library/GuiLibHelper.java
+++ src/main/java/com/gildedgames/orbis/lib/client/gui/util/gui_library/GuiLibHelper.java
@@ -1,17 +1,76 @@
 package com.gildedgames.orbis.lib.client.gui.util.gui_library;
 
+import com.gildedgames.orbis.lib.client.rect.Pos2D;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
 public class GuiLibHelper
 {
-	public static List<IGuiElement> getAllChildrenRecursivelyFor(IGuiElement element)
+	public static void alignVertically(final IGuiViewer viewer, final Pos2D origin, final float yPadding, final GuiElement... elements)
 	{
-		List<IGuiElement> children = Lists.newArrayList();
+		GuiElement lastElement = null;
+		for (final GuiElement e : elements)
+		{
+			final float yOffset = lastElement != null ? lastElement.dim().maxY() + yPadding : origin.y();
+
+			if (!e.state().hasBuilt())
+			{
+				e.build(viewer);
+			}
+
+			e.dim().mod().x(origin.x()).y(yOffset).flush();
+
+			lastElement = e;
+		}
+	}
+
+	/**
+	 * Will set the min position and max positiot (width/height) based on children inside.
+	 * @param topLevel
+	 */
+	public static void assembleMinMaxArea(final IGuiElement topLevel)
+	{
+		float maxX = Float.MIN_VALUE;
+		float minX = Float.MAX_VALUE;
+		float minY = Float.MAX_VALUE;
+		float maxY = Float.MIN_VALUE;
+
+		final List<IGuiElement> elements = Lists.newArrayList(topLevel.context().getChildren());
+		elements.add(topLevel);
+
+		for (final IGuiElement child : elements)
+		{
+			if (child.dim().maxX() > maxX)
+			{
+				maxX = child.dim().maxX();
+			}
+
+			if (child.dim().min().x() < minX)
+			{
+				minX = child.dim().min().x();
+			}
+
+			if (child.dim().maxY() > maxY)
+			{
+				maxY = child.dim().maxY();
+			}
+
+			if (child.dim().min().y() < minY)
+			{
+				minY = child.dim().min().y();
+			}
+		}
+
+		topLevel.dim().mod().x(minX).y(minY).width(maxX - minX).height(maxY - minY).flush();
+	}
+
+	public static List<IGuiElement> getAllChildrenRecursivelyFor(final IGuiElement element)
+	{
+		final List<IGuiElement> children = Lists.newArrayList();
 
 		// Prevents from adding the top element itself
-		for (IGuiElement child : element.context().getChildren())
+		for (final IGuiElement child : element.context().getChildren())
 		{
 			fetchAllChildrenRecursivelyFor(children, child);
 		}
@@ -19,11 +78,11 @@ public class GuiLibHelper
 		return children;
 	}
 
-	private static void fetchAllChildrenRecursivelyFor(List<IGuiElement> addTo, IGuiElement top)
+	private static void fetchAllChildrenRecursivelyFor(final List<IGuiElement> addTo, final IGuiElement top)
 	{
 		addTo.add(top);
 
-		for (IGuiElement child : top.context().getChildren())
+		for (final IGuiElement child : top.context().getChildren())
 		{
 			fetchAllChildrenRecursivelyFor(addTo, child);
 		}
